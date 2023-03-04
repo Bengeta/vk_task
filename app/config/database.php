9e756b0549712b310da5e67ff38c70f436dc8969
Bengeta <inovozhilov08@gmail.com>
2023-03-05 05:45:51 +1000

    vk_task:start working

---
+++ app/config/database.php
@@ -0,0 +1,12 @@
+'mysql' => array(
+    'read' => array(
+        'host' => '127.0.0.1',
+    ),
+    'write' => array(
+        'host' => '127.0.0.1'
+    ),
+    'driver'    => 'mysql',
+    'database'  => 'test_task',
+    'username'  => 'sail',
+    'password'  => 'password',
+),
